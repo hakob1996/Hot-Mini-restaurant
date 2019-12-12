afd8080c7993644cd2670bb1668efb43e75cc455
Hakob Muradyan <hakobmuradyan1996@gmail.com>
2019-12-11 18:29:44 -0800

    status

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var waiting = [];
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "main.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/add", function(req, res) {
